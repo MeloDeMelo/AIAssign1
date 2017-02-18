60ba0b54bdf64e3bbeb6e74c3e13c7555d77105b
Max DeMelo <max.demelo2267@gmail.com>
2017-02-18 17:43:09 -0500

    BridgeTorch Base + tests

--- src/Assign1/BridgeTorch.java
+++ src/Assign1/BridgeTorch.java
@@ -1,13 +1,84 @@
 package Assign1;
 
+import java.util.ArrayList;
+
 /**
- * Created by Max on 2/18/2017.
+ * Bridge and Torch problem base
+ * Written by Max DeMelo - 100937368
+ * 2/18/2017
  */
 public class BridgeTorch {
 
-    public int[] startingPeople;
+    //Left of bridge means not crossed (LoB)
+    //Right of bridge means has crossed (RoB)
+    private ArrayList peopleLoB, peopleRoB;
+    private boolean torchLeft;
+    private int numberOfPeople;
 
-    public BridgeTorch(int[] startingPeople){
-        this.startingPeople = startingPeople;
+    public BridgeTorch(ArrayList peopleLoB){
+        this.peopleLoB = peopleLoB;
+        numberOfPeople = peopleLoB.size();
+        peopleRoB = new ArrayList();
+        torchLeft = true;
+    }
+
+    public int move(ArrayList peopleMoving){
+        if (peopleMoving.size() == 1){
+            if (torchLeft){
+                if(peopleLoB.contains(peopleMoving.get(0))){
+                    peopleLoB.remove(peopleMoving.get(0));
+                    peopleRoB.add(peopleMoving.get(0));
+                }
+                else{
+                    return -1;
+                }
+            }
+            else {
+                if(peopleRoB.contains(peopleMoving.get(0))){
+                    peopleRoB.remove(peopleMoving.get(0));
+                    peopleLoB.add(peopleMoving.get(0));
+                }
+                else{
+                    return -1;
+                }
+            }
+            toggleTorch();
+            return (int) peopleMoving.get(0);
+        }
+        else if((peopleMoving.size() == 2) && (torchLeft)){
+            int longest = -1;
+            if(peopleLoB.contains(peopleMoving.get(0))){
+                peopleLoB.remove(peopleMoving.get(0));
+                peopleRoB.add(peopleMoving.get(0));
+                if(peopleLoB.contains(peopleMoving.get(1))){
+                    peopleLoB.remove(peopleMoving.get(1));
+                    peopleRoB.add(peopleMoving.get(1));
+                    toggleTorch();
+                    return ((int)peopleMoving.get(0) > (int)peopleMoving.get(1)) ? (int)peopleMoving.get(0) : (int)peopleMoving.get(1);
+                }
+            }
+            return longest;
+        }
+        return -1;
+    }
+
+    public boolean checkWin(){
+        return (peopleLoB.isEmpty()) && (peopleRoB.size() == numberOfPeople) && (!torchLeft);
+    }
+
+    private void toggleTorch(){
+        torchLeft = !torchLeft;
+    }
+
+    public boolean getTorch(){
+        return torchLeft;
+    }
+
+    public ArrayList getPeopleLoB(){
+        return peopleLoB;
+    }
+
+    public ArrayList getPeopleRoB(){
+        return peopleRoB;
     }
 }
--- src/test/BridgeTorchTest.java
+++ src/test/BridgeTorchTest.java
@@ -1,14 +1,71 @@
 package test;
 
+import Assign1.BridgeTorch;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Max on 2/18/2017.
+ * Tests for Bridge and Torch base
+ * Written by Max DeMelo - 100937368
+ * 2/18/2017
  */
 public class BridgeTorchTest {
+
+    private BridgeTorch bt;
+    private ArrayList startup, move;
+    private int distance;
+
     @org.junit.Before
     public void setUp() throws Exception {
-
+        startup = new ArrayList();
+        move = new ArrayList();
+        startup.add(1);
+        startup.add(2);
+        startup.add(3);
+        startup.add(5);
+        bt = new BridgeTorch(startup);
+        distance = 0;
     }
 
+    @Test
+    public void moveForward() {
+        move.add(1);
+        move.add(3);
+        distance = bt.move(move);
+        assertEquals(distance, 3);
+        assertEquals(move, bt.getPeopleRoB());
+        assertFalse(bt.getTorch());
+    }
+
+    @Test
+    public void moveBack() {
+        move.add(1);
+        distance = bt.move(move);
+        assertFalse(bt.getPeopleLoB().contains(1));
+        distance = bt.move(move);
+        assertEquals(distance, 1);
+        assertTrue(bt.getPeopleLoB().contains(1));
+    }
+
+    @Test
+    public void checkWin() {
+        int total = 0;
+        move.add(1);
+        move.add(2);
+        total += bt.move(move);
+        assertFalse(bt.checkWin());
+        move.remove(1);
+        total += bt.move(move);
+        move.add(3);
+        total += bt.move(move);
+        move.remove(1);
+        total += bt.move(move);
+        move.add(5);
+        total += bt.move(move);
+        assertTrue(bt.checkWin());
+        assertEquals(total, 12);
+    }
 }
